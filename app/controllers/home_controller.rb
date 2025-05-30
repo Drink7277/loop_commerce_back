51c91e0f1fae6497c77b15c450e0c69a52e24ea4
Drink <drink7277@gmail.com>
2025-05-30 16:49:50 +0700

    fix style

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    frontend_url = ENV['FRONTEND_URL'] || 'http://localhost:8080/'
+    frontend_url = ENV["FRONTEND_URL"] || "http://localhost:8080/"
     redirect_to frontend_url, allow_other_host: true
   end
 end
